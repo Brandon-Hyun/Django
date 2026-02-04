8699a5df48643374ca533361db4ffe664091cde1
Brandon Hyun <>
2026-02-04 13:53:12 +0900

    Day04

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,16 @@
+# Django가 기본 제공하는 User 모델 import
+# But, 프로젝트에서 AUTH_USER_MODEL 설정을 통해 커스텀 User 모델을 정의할 수 있음
+# 커스텀 User 모델을 쓰고 있는데 기본 User를 직접 import 하면 !!실제로 존재하지 않는 모델을 참조!!
+# from django.contrib.auth.models import User
+
+# 현재 프로젝트에서 실제로 사용 중인 User 모델을 반환하는 함수/ 기본 User or 커스텀 User 둘 다 가능
+# Django 공식 문서에서도 User 모델을 참조할 때는 항상 get_user_model()을 쓰라고 권장함.
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 from django.db import models
+
+
 
 # 제목
 # 본문
@@ -17,6 +29,12 @@ class Blog(models.Model):
     category = models.CharField('카테고리', max_length=10, choices=CATEGORY_CHOICES)
     title = models.CharField('제목', max_length=100)
     content = models.TextField('본문')
+    # 외래키 on_delete=
+    # models.CASCADE 참조된 부모 데이터 삭제 시 자식 데이터도 함께 연쇄 삭제
+    # models.PROTECT 참조된 부모 객체를 삭제 시도,자식 데이터가 있으면
+    #                ProtectedError를 발생시켜 삭제를 막음. 참조 데이터 보호가 필요 시 무결성 지킴이
+    # models.SET_NULL 널 값을 넣음 -> 유저 삭제시 블로그의 author 가 null이 됨. (모델에서 Null = True 여야 함.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField('작성일자', auto_now_add=True)
     updated_at = models.DateTimeField('수정일자', auto_now=True)
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,93 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
+
+from blog.forms import BlogForm
 from blog.models import Blog
 
 def blog_list(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.all().order_by('-created_at')
 
-    # 가져온 쿠키 중 key 'visit' 값 가져오고 +1, 기본값 0.
-    visits = int(request.COOKIES.get('visits',0)) + 1
+    # 제목과 본문 모두 검색 대상으로 설정
+    q = request.GET.get('q')
+    if q:
+        blogs = blogs.filter(
+            Q(title__icontains=q) |
+            Q(content__icontains=q)
+        )
+        # blogs = blogs.filter(content__icontains=q)
 
-    # 서버 세션에 키 'count' 저장, 이전 값 없으면 0 시작, 매 요청마다 +1
-    request.session['count'] = request.session.get('count', 0) + 1
+    paginator = Paginator(blogs, 9)
+    page = request.GET.get('page')
+    page_object = paginator.get_page(page)
+
+    # # 가져온 쿠키 중 key 'visit' 값 가져오고 +1, 기본값 0.
+    # visits = int(request.COOKIES.get('visits',0)) + 1
+
+    # # 서버 세션에 키 'count' 저장, 이전 값 없으면 0 시작, 매 요청마다 +1
+    # request.session['count'] = request.session.get('count', 0) + 1
 
     context = {
-        'blogs':blogs,
-        'count':request.session['count']
+        # 'blogs':blogs,
+        # 'count':request.session['count'],
+        'page_object':page_object,
     }
 
-    response = render(request, 'blog_list.html', context)
-    # HTTP 응답 객체에 쿠키 헤더를 추가, 키 값:'visits'
-    # Django의 HttpResponse 객체는 self.cookies라는 쿠키저장소, Dict 같음..
-    response.set_cookie('visits',visits)
+    # response = render(request, 'blog_list.html', context)
+    # # HTTP 응답 객체에 쿠키 헤더를 추가, 키 값:'visits'
+    # # Django의 HttpResponse 객체는 self.cookies라는 쿠키저장소, Dict 같음..
+    # response.set_cookie('visits',visits)
 
-    return response
+    return render(request,'blog_list.html',context)
 
 def blog_detail(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
     context = {'blog':blog}
     return render(request, 'blog_detail.html', context )
+
+@login_required()
+def blog_create(request):
+    # if request.user.is_authenticated:
+    #     return redirect('login')
+
+    form = BlogForm(request.POST or None)
+    if form.is_valid():
+        blog = form.save(commit=False)
+        blog.author = request.user
+        blog.save()
+        return redirect(reverse('blog_detail',kwargs= {'pk': blog.pk} ))
+
+    context = {'form':form}
+    return render(request, 'blog_create.html',context)
+
+@login_required()
+def blog_update(request, pk):
+    # if request.user != blog.author:
+    #     raise Http404
+    blog = get_object_or_404(Blog, pk=pk, author=request.user)
+
+    form = BlogForm(request.POST or None, instance=blog)
+    if form.is_valid():
+        blog = form.save()
+        return redirect(reverse('blog_detail',kwargs={'pk': blog.pk}))
+
+    context = {
+        'form':form,
+    }
+
+    return render(request,'blog_update.html',context)
+
+@login_required()
+# 삭제나 수정은 GET 아니고 POST요청으로 받아야함 (검색엔진,캐시서버, 유저가 뒤로고침,새로고침 할 수있음)
+# GET 요청은 폼 제출이나 인증 절차 없어도 실행됨.,
+# require_http_methods는 리스트로 받는게, 메서드가 GET, POST 둘 다 허용할수도,, 복수 가능!!
+@require_http_methods(['POST'])
+def blog_delete(request, pk):
+    blog = get_object_or_404(Blog, pk=pk, author=request.user)
+    blog.delete()
+
+    return redirect(reverse('blog_list'))
+
